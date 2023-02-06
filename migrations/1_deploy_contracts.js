e6ecca44432790f147f810707e76ec322e79d9d0
Энхтөр <enxtur.mon@gmail.com>
2023-02-07 00:20:59 +0800

    adds test members

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,6 +1,10 @@
 const MultisigWallet = artifacts.require("MultisigWallet");
 
-let members = ["0x29fa1BCB4B73CA4F6A01b882303C1210256C2A9B"]
+let members = [
+  "0x29fa1BCB4B73CA4F6A01b882303C1210256C2A9B", // enxtur
+  "0x6865fB489b645Cc703B5ab3552a3B55149E4F92B", // bilgee 1
+  "0x5d8a68e8c2321Ec1DB810aB666c6DFf55107C030", // bilgee 2
+]
 
 module.exports = function(deployer, network, accounts) {
   if (network === "test") {
